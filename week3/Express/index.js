b394e6485a295b9b020d918a4e2be33f107fa9ca
MrDanielHo <daniel.kun.tin.ho@gmail.com>
2025-04-10 17:11:50 +0100

    updated gitnore for exercise folders

--- week3/Express/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app')
-const port = 3000
-
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-})
--- week3/connect_database/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const api = require('./api')
-const port = process.env.PORT
-
-api.listen (port, () => {
-    console.log(`API lisening on port ${port}`)
-})
